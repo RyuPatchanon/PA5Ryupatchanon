3a610afab0965df97733b10f782743826a5cc933
RyuPatchanon <ryupatchanon@gmail.com>
2023-12-06 16:36:45 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import openai
 import pandas as pd
-from newspaper import Article
+import json
 
 # Set up the OpenAI API key input in the sidebar
 st.sidebar.title('OpenAI API Key')
@@ -10,46 +10,44 @@ api_key = st.sidebar.text_input('Enter your OpenAI API Key:', type='password')
 # Set the OpenAI API key
 openai.api_key = api_key
 
-def extract_text_from_url(article_url):
-    article = Article(article_url)
-    article.download()
-    article.parse()
-    return article.text
-
-def summarize_text_with_gpt3(text):
+def get_recommendations(user_preferences):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Summarize the text."},
-            {"role": "user", "content": text}
+            {"role": "system", "content": "Recommend movies or shows based on my preferences."},
+            {"role": "user", "content": user_preferences}
         ]
     )
     return response.choices[0].message['content']
 
+def format_recommendations(recommendations):
+    recommendations_list = []
+    # Split the recommendations by lines
+    recommendation_lines = recommendations.splitlines()
+    for line in recommendation_lines:
+        # Split each line into name and reason
+        name, reason = line.split(' - ')
+        # Create a dictionary for each recommendation
+        recommendation = {'Name': name.strip(), 'Reason': reason.strip()}
+        recommendations_list.append(recommendation)
+    return recommendations_list
+
 def main():
-    st.title('Article Text Extractor and Summarizer with OpenAI GPT-3')
+    st.title('Movie or Show Recommendation Generator')
 
-    # Input field for article URL
-    article_url = st.text_input('Enter the URL of the news article:')
+    # Input field for user preferences
+    user_preferences = st.text_area('Enter your movie or show preferences:')
 
-    if st.button('Extract and Summarize'):
-        if article_url:
+    if st.button('Get Recommendations'):
+        if user_preferences:
             try:
-                extracted_text = extract_text_from_url(article_url)
-                summary = summarize_text_with_gpt3(extracted_text)
+                recommendations = get_recommendations(user_preferences)
+                recommendations_list = format_recommendations(recommendations)
 
-                st.subheader('Extracted Text:')
-                st.write(extracted_text)
-
-                st.subheader('Summary:')
-                st.write(summary)
-
-                # Display the extracted text and summary in a Pandas DataFrame
-                df = pd.DataFrame({'Extracted Text': [extracted_text], 'Summary': [summary]})
-                st.subheader('DataFrame:')
-                st.write(df)
+                st.subheader('Recommendations:')
+                st.json(recommendations_list)
             except Exception as e:
-                st.error('An error occurred. Please check the URL or try again.')
+                st.error('An error occurred. Please try again.')
                 st.error(f'Error details: {str(e)}')
 
 if __name__ == '__main__':
