4b0f92e7da358b1804e781dc1d0306bfd5505d4c
RyuPatchanon <ryupatchanon@gmail.com>
2023-12-07 13:13:13 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,52 +1,44 @@
 import streamlit as st
 import openai
+import json
 import pandas as pd
 
-# Set up the OpenAI API key input in the sidebar
-st.sidebar.title('OpenAI API Key')
+# Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
-client = openai.OpenAI(api_key=user_api_key)
 
-def get_recommendations(user_preference):
-    response = client.chat.completion.create(
+client = openai.chat.completion.create(api_key=user_api_key)
+
+prompt = """Act as a movie enthusiast. You will receive a 
+            a description of preference and you should give out movie or show recommendations based on the given preference.
+            List the recommendations in a 4 column table, one recommendation per line.
+            Each recommendation should have 4 fields:
+            - "Name" - the name of the recommended show or movie.
+            - "Reason" - The reason the movie or show is recommended based on user preference.
+            - "Genre" - the genre of the movie or show
+            - "Notable" - Notable actors or directors involved, list at most 3 names
+            Don't say anything at first. Wait for the user to say something.
+        """
+
+st.title('Movie/Show Recommendations')
+st.markdown('Enter your preferences, and the AI will provide recommendations.')
+
+user_preference = st.text_area("Enter your preferences:", "I enjoy action-packed movies with suspense.")
+
+# submit button after preference input
+if st.button('Get Recommendations'):
+    response = client.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": user_preference},
-            {"role": "user", "content": ""}
+            {"role": "system", "content": prompt},
+            {'role': 'user', 'content': user_preference},
         ]
     )
-    return response.choices[0].message['content']
+    
+    # Show the response from the AI in a box
+    st.markdown('**AI Recommendations:**')
+    recommendation_dictionary = response['choices'][0]['message']['content']
 
-def format_recommendations(recommendations):
-    recommendations_list = []
-    # Split the recommendations by lines
-    recommendation_lines = recommendations.splitlines()
-    for line in recommendation_lines:
-        # Split each line into name and summary
-        name, summary = line.split(' - ')
-        # Create a dictionary for each recommendation
-        recommendation = {'Name': name.strip(), 'Summary': summary.strip()}
-        recommendations_list.append(recommendation)
-    return recommendations_list
+    rec = json.loads(recommendation_dictionary)
+    recommendation_df = pd.DataFrame.from_dict(rec)
 
-def main():
-    st.title('Movie or Show Recommendation Generator')
-
-    # Input field for user preferences
-    user_preference = st.text_area('Enter your preference:')
-
-    if st.button('Get Recommendations'):
-        if user_preference:
-            try:
-                recommendations = get_recommendations(user_preference)
-                recommendations_list = format_recommendations(recommendations)
-
-                # Display the recommendations in a Pandas DataFrame with 2 columns
-                df = pd.DataFrame(recommendations_list)
-                st.write(df)
-            except Exception as e:
-                st.error('An error occurred. Please try again.')
-                st.error(f'Error details: {str(e)}')
-
-if __name__ == '__main__':
-    main()
+    st.table(recommendation_df)
