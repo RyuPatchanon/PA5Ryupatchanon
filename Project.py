9a0c0464a5f52f4b5b1ce9927410dca82b541d51
RyuPatchanon <ryupatchanon@gmail.com>
2023-12-07 14:41:56 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -4,14 +4,18 @@ import openai
 import json
 
 def main():
+    st.set_page_config(
+    page_title="Movie/Show Recommendation App",
+    page_icon="🎬",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
     st.sidebar.header("OpenAI API Key")
     user_api_key = st.sidebar.text_input("Enter your OpenAI API Key")
     
     if user_api_key:
         client = openai.OpenAI(api_key=user_api_key)
         st.write("API Key Set Successfully!")
-
-        st.title("Movie/Show Recommendation App")
 
         user_preference = st.text_input("Enter your preference:")
         prompt = f"Act as a movie enthusiast. You will receive a description of preference and you should give out movie or show recommendations based on the given preference. List the recommendations in a Json array, one recommendation per line. Each recommendation should have 4 fields: - 'Name' - the name of the recommended show or movie. - 'Reason' - The reason the movie or show is recommended based on user preference. - 'Genre' - the genre of the movie or show - 'Notable' - Notable actors or directors involved, list at most 3 names. Preference: {user_preference}"
@@ -33,20 +37,6 @@ def main():
             st.table(suggestion_df)
     else:
         st.warning("Please enter your OpenAI API Key in the sidebar.")
-
-    # Add custom CSS for background color and style
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #3F250B; /* Dark brown color */
-            color: #ffffff;
-            font-family: Arial, sans-serif;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
+    
 if __name__ == "__main__":
     main()
