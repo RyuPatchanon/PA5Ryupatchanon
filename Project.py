02bb518df9f44823ca89351734f21f2f780acf66
RyuPatchanon <ryupatchanon@gmail.com>
2023-12-07 13:52:45 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,30 +2,10 @@ import streamlit as st
 import pandas as pd
 import openai
 
-
-def generate_recommendations(user_preference, client):
-    # GPT-3 prompt to generate recommendations
-    prompt = f"Act as a movie enthusiast. You will receive a description of preference and you should give out movie or show recommendations based on the given preference. List the recommendations in a 4 column table, one recommendation per line. Each recommendation should have 4 fields: - 'Name' - the name of the recommended show or movie. - 'Reason' - The reason the movie or show is recommended based on user preference. - 'Genre' - the genre of the movie or show - 'Notable' - Notable actors or directors involved, list at most 3 names. Preference: {user_preference}"
-    
-    # Use GPT-3 to generate recommendations
-    messages_so_far = [
-        {"role": "system", "content": prompt},
-        {'role': 'user', 'content': user_preference},
-    ]
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages_so_far
-    )
-    
-    recommendations = response.choices[0].text.strip().split("\n")
-    recommendations = [rec.split(",") for rec in recommendations]
-    
-    return recommendations
-
 def main():
     st.sidebar.header("OpenAI API Key")
     user_api_key = st.sidebar.text_input("Enter your OpenAI API Key")
-
+    
     if user_api_key:
         client = openai.OpenAI(api_key=user_api_key)
         st.write("API Key Set Successfully!")
@@ -33,9 +13,20 @@ def main():
         st.title("Movie/Show Recommendation App")
 
         user_preference = st.text_input("Enter your preference:")
-
+        prompt = f"Act as a movie enthusiast. You will receive a description of preference and you should give out movie or show recommendations based on the given preference. List the recommendations in a 4 column table, one recommendation per line. Each recommendation should have 4 fields: - 'Name' - the name of the recommended show or movie. - 'Reason' - The reason the movie or show is recommended based on user preference. - 'Genre' - the genre of the movie or show - 'Notable' - Notable actors or directors involved, list at most 3 names. Preference: {user_preference}"
+        st.markdown('Input your preference for movie or show. \n\
+            The AI will give you suggestions on what to watch.')
         if st.button("Get Recommendations"):
-            recommendations = generate_recommendations(user_preference)
+            messages_so_far = [
+                {"role": "system", "content": prompt},
+                {'role': 'user', 'content': user_preference},]
+
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=messages_so_far)
+            
+            recommendations = response.choices[0].text.strip().split("\n")
+            recommendations = [rec.split(",") for rec in recommendations]
             df = pd.DataFrame(recommendations, columns=["Name", "Reason", "Genre", "Notable"])
             st.dataframe(df)
     else:
